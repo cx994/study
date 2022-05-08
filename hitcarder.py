c50356296a9c1cd8eb001a53a7c2629d4733d56f
cyc <81349137+cx994@users.noreply.github.com>
2022-05-08 08:44:39 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -8,6 +8,7 @@ import time
 import datetime
 import os
 import sys
+import ddddocr
 import message
 
 
@@ -20,6 +21,7 @@ class HitCarder(object):
         login_url: (str) 登录url
         base_url: (str) 打卡首页url
         save_url: (str) 提交打卡url
+        captcha_url:(str) 验证码url
         sess: (requests.Session) 统一的session
     """
 
@@ -29,6 +31,7 @@ class HitCarder(object):
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.captcha_url = "https://healthreport.zju.edu.cn/ncov/wap/default/code"
         self.sess = requests.Session()
         self.sess.keep_alive = False
         retry = Retry(connect=3, backoff_factor=0.5)
@@ -71,7 +74,16 @@ class HitCarder(object):
         time.sleep(1)
         res = self.sess.post(self.save_url, data=self.info)
         return json.loads(res.text)
-
+    
+    def get_captcha(self):
+        """Get CAPTCHA code"""
+        cookie_dict = {'eai-sess': '9k1adtbd8ti87t488f786vvtl3'}
+        self.sess.cookies = requests.cookies.cookiejar_from_dict(cookie_dict)
+        resp = self.sess.get(self.captcha_url)
+        captcha = self.ocr.classification(resp.content)
+        print("验证码：", captcha)
+        return captcha
+    
     def get_date(self):
         """Get current date."""
         today = datetime.datetime.utcnow() + datetime.timedelta(hours=+8)
@@ -143,6 +155,7 @@ class HitCarder(object):
         new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 入境
         new_info['sfqrxxss'] = 1 # 属实
         new_info['campus'] = old_info['campus']
+        new_info['captcha'] = 
 
         self.info = new_info
         # print(json.dumps(self.info))
